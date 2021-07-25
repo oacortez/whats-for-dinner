51cd4d06cc6e1329cdc25d53a00afb52d361bac4
Oscar Cortez <oacortez98@gmail.com>
2021-07-25 15:25:25 -0600

    Refactor main.js file clean out comments and add spacing

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-// var cookingPot.querySelector()
-// we are going to have 2 helper functions
-// 1 function is going to be "hide" and the other is going to be "show"
-// google - javascrip - "classList.add" and "classList.remove"
-// google - css = display: none
-// google - event.default
-
-
-// querySelector ⬇️
 var sideDishInput = document.querySelector('#sideDish');
 var mainDishInput = document.querySelector('#mainDish');
 var dessertsInput = document.querySelector('#desserts');
@@ -18,13 +9,10 @@ var youShouldMakeText = document.querySelector('#recipeText');
 var deleteDishBtn = document.querySelector('#removeRecipe');
 
 
-// EventListeners ⬇️
 letsCookBtn.addEventListener('click', getDish);
 deleteDishBtn.addEventListener('click', deleteDish)
 
 
-
-// Functions ⬇️
 function getRandomFoodIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
@@ -66,19 +54,3 @@ function deleteDish() {
   deleteDishBtn.classList.add('hidden');
   letsCookBtn.disabled = false;
 }
-
-// if(sideDishRadioBtn.checked) {
-//   // statment of sidedish array will come here
-// }
-// var letsCookBtn = document.querySelector('#letsCookBtn');
-//
-// letsCookBtn.addEventListener('click', test);
-//
-// var test1 = document.querySelector('#test1')
-//
-// function test() {
-//   event.preventDefault();
-//   sideDishRadioBtn.value;
-//   console.log(sideDishRadioBtn.value);
-//   console.log("It Works!", test1.value);
-// }
